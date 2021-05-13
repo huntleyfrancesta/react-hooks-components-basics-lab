c138a8c4ea9dd95e9bcb8c22d3d9f779d15ce903
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-05-13 15:21:21 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,22 @@
-import React from "react";
+);
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
 }
 
-function Home() {
+function About() {
   return (
-    <div id="home">
-      <h1>Home</h1>
+    <div id="about">
+      <h1>About</h1>
     </div>
-  );
+  )
 }
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+       <Home />
+       <About />
     </div>
   );
 }
